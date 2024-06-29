3d9f2b2f31f9eb722e6765c9006c70b1471b6b3d
Isaias Junior <isaiscastro56@gmail.com>
2024-06-29 20:50:54 -0300

    calculando o valor do produto

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 function adicionar() {
    // recuperar os valores do produto, quantidade e valor
 
-   let nomeProduto = document.querySelector('#produto');
-   let quantidade = document.querySelector('#quantidade')
+   let produto = document.querySelector('#produto').value;
+   let nomeProduto = produto.split('-')[0];
+   let valorUnitario = produto.split('R$')[1];
+   let quantidade = document.querySelector('#quantidade');
 
+   alert(nomeProduto)
+   alert(valorUnitario)
+   alert(quantidade)
+   let preco = quantidade.value * valorUnitario;
+   alert(preco);
    // calcular o preço, o subtotal
    // adicionar no carrinho
    // atualizar o valor total da compra
