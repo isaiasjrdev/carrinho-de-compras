6c2ab285bd74d25c6f600491b756f979b2d1178f
Isaias Junior <isaiscastro56@gmail.com>
2024-06-29 20:41:37 -0300

    captura do produto

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function adicionar() {
+   // recuperar os valores do produto, quantidade e valor
+
+   let nomeProduto = document.querySelector('#produto');
+   let quantidade = document.querySelector('#quantidade')
+
+   // calcular o preço, o subtotal
+   // adicionar no carrinho
+   // atualizar o valor total da compra
+
+}
+
+function limpar() {
+   
+}
