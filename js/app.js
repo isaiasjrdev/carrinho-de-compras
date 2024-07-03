93fa1ab6b0bee12a38a36a6f8677d8a04daa3da7
Isaias Junior <isaiscastro56@gmail.com>
2024-07-03 19:40:23 -0300

    alteração no campo do total

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+let totalGeral = 0
+document.querySelector('#lista-produtos').innerHTML = '';
+document.querySelector('#valor-total').textContent = 'R$ 0';
+
 function adicionar() {
    // recuperar os valores do produto, quantidade e valor
    let produto = document.querySelector('#produto').value;
@@ -13,9 +17,10 @@ function adicionar() {
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>
    `
-
    // atualizar o valor total da compra
-
+   totalGeral = totalGeral + preco;
+   let campoTotal = document.querySelector('#valor-total')
+   campoTotal.textContent = `R$ ${totalGeral}`
 }
 
 function limpar() {
