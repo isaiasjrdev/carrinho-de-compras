3dcf1e7582ce5117146c9df0496d1f3bf934e9f0
Isaias Junior <isaiscastro56@gmail.com>
2024-07-03 19:34:38 -0300

    adicionando o produto ao carrinho

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
 function adicionar() {
    // recuperar os valores do produto, quantidade e valor
-
    let produto = document.querySelector('#produto').value;
    let nomeProduto = produto.split('-')[0];
    let valorUnitario = produto.split('R$')[1];
-   let quantidade = document.querySelector('#quantidade');
-
-   alert(nomeProduto)
-   alert(valorUnitario)
-   alert(quantidade)
-   let preco = quantidade.value * valorUnitario;
-   alert(preco);
+   let quantidade = document.querySelector('#quantidade').value;
    // calcular o preço, o subtotal
-   // adicionar no carrinho
+   let preco = quantidade * valorUnitario;
+   let carrinho = document.querySelector('#lista-produtos');
+      // adicionar no carrinho
+   carrinho.innerHTML = carrinho.innerHTML + `
+   <section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>
+   `
+
    // atualizar o valor total da compra
 
 }
 
 function limpar() {
-   
+
 }
